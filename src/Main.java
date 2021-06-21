a573edd611a423fb087f581559972bacc3b70450
Tchorti <tchelo.rock@gmail.com>
2021-06-21 19:04:03 -0300

    Condĩção if e else

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Apenas um teste!
+
+		
+		int a, b, c, soma;
+		
+		a = 10;
+		b = 10;
+		c = 50;
+		
+		soma = a * b / c;
+		
+		System.out.println("o Valor da conta é = " + soma);
+		
+		if (a < b) {
+			System.out.println("o valor de a é maior que b");
+		}
+		
+		if (a > b) {
+			System.out.println("O valor de b é maior que a");
+		}
+ 
+		else {
+			System.out.println("O valor de c é maior que a e b ");
+		}
+	
+
+	}
+
+}
