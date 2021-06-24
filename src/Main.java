e057482d61fef1e7cd0d71809e55b8e44d25d4a2
“Tchorti” <tchelo.rock@gmail.com>
2021-06-24 18:46:37 -0300

    contasjava

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,46 @@
-import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		String  nome =  "Tchelo";
-		int idade = 15;
-		double altura = 1.74;
-		double salario = 5000.0;
-		
-		System.out.printf("%s tem de idade %d e de altura %.2f recebe o valor de R$ %.2f %n", nome, idade, altura, salario);
-		
-		Locale.setDefault(Locale.US);
-		
-		System.out.printf("%s tem de idade %d e de altura %.2f recebe o valor de R$ %.2f %n", nome, idade, altura, salario);
-		
-		if (idade < 18) {
-			System.out.println("Não pode entrar");
-		}
-		
-		else {
-			
-			System.out.println("Pode entrar");
-		}
-		
-		while (idade < 18) {
-			System.out.println (idade);
-			idade = idade + 1;
-		}
-		
-		int a, b, c, soma;
-		
+		// Fazendo apenas um teste de fixação//
+
+		String nome = "Tchelo";
+		int idade = 33;
+		double renda = 5000.0;
+
+		System.out.printf(" %s tem de idade %d e recebe o salário de R$ %.2f ao mês \n ", nome, idade, renda);
+
+		int x, y;
+
+		x = 10;
+		y = 20;
+		System.out.println("O valor da conta é = " + (x + y));
+
+		// montando a conta de outra forma//
+
+		int a, b, soma;
 		a = 10;
 		b = 20;
-		c = 30;
+
+		soma = a + b;
+
+		System.out.println(" O valor da conta é = " + soma);
 		
-		soma = a * b / c;
+		int idade1 = 17;
 		
-		System.out.println("O valor da soma é = " + soma);
-		
-		while (a < b) {
-			System.out.println(a);
-			a = a + 0;
+		if (idade1 < 17){
+		System.out.println("Pode entrar");
+		}
+		else {
+			System.out.println(" Não pode entrar");
 		}
 		
-		
-		
+		int i = 0;
+		while (i < 5) {
+			System.out.println(i);
+			i++;
 		}
 	}
 
+}
